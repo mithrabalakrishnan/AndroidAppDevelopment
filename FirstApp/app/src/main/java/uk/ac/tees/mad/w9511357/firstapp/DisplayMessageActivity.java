d3c490e32e96c67bf8738ed48472d0229f23091d
unknown <W9511357@live.tees.ac.uk>
2021-02-12 22:34:45 +0530

    First app commited

---
+++ FirstApp/app/src/main/java/uk/ac/tees/mad/w9511357/firstapp/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package uk.ac.tees.mad.w9511357.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView3);
+        textView.setText(message);
+    }
+}
